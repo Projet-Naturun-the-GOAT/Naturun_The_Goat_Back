a5d0e02653fa77bb7eb5993b7db4ed945c6f00ae
Nathan Bapin <bapin.nathan@gmail.com>
2025-10-16 14:12:47 +0200

    reset l'environnement en même temps que l'ia

--- src/main.py
+++ src/main.py
@@ -79,6 +79,7 @@ def main(
                     print("ℹ️  Aucun modèle existant à supprimer.")
                 
                 # 🔁 Recréation complète de l’agent
+                env.reset()
                 agent = QLearningAgent(env)
                 agent.train(
                     env,
